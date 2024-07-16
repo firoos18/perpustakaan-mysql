24af74ea4d3be463951ef0a3e2b11ca31d21f9f2
firoos18 <itsfiroos@gmail.com>
2024-07-16 12:19:32 +0700

    bug : initmysql

--- helpers/init_mysql.js
+++ helpers/init_mysql.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "mysql",
+    dialect: require("mysql2"),
     pool: {
       max: 5,
       min: 0,
